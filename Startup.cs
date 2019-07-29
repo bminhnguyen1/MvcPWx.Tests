1034556a58795fc3bb4fef0e6536363ea55ca5e7
buinguyenminhnguyenitc <guunee10@gmail.com>
2019-07-29 12:36:16 +0700

    update

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcPWx.Tests.Startup))]
+namespace MvcPWx.Tests
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
